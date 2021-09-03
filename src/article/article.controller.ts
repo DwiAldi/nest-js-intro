4ef183d355d26fef2da4966899a820052ad50177
Dwi Aldi <aldidwi54@gmail.com>
2021-09-03 22:39:24 +0700

    Adding Documentation
    Set API Documentation.
    
    How to Access : localhost:3000/api

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,11 +1,30 @@
 import { Controller, Post, Body, Get, Param, Patch, Delete} from "@nestjs/common";
+import { ApiCreatedResponse, ApiForbiddenResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { ArticleService } from "./article.service";
 
+@ApiTags('Article')
 @Controller('article')
 export class ArticleController{
     constructor(private readonly articleService: ArticleService){}
 
+    @Get()
+    @ApiOkResponse({ description: 'The Resource list has been successfully returned.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
+    async getAllArticle(){
+        const articles = await this.articleService.getArticle();
+        return articles;
+    }
+    
+    @Get(':id')
+    @ApiOkResponse({ description: 'The Resource has been successfully returned.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
+    getArticle(@Param('id') articleID: number){
+        return this.articleService.getSingleArticle(articleID)
+    }
+
     @Post()
+    @ApiCreatedResponse({ description: 'The Resource has been successfully created.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
     async addArticle(
             @Body('title') articleTitle: string, 
             @Body('body') articleBody: string,
@@ -15,18 +34,9 @@ export class ArticleController{
         return{id: generatedID};
     }
 
-    @Get()
-    async getAllArticle(){
-        const articles = await this.articleService.getArticle();
-        return articles;
-    }
-    
-    @Get(':id')
-    getArticle(@Param('id') articleID: number){
-        return this.articleService.getSingleArticle(articleID)
-    }
-
     @Patch(':id')
+    @ApiOkResponse({ description: 'The Resource has been successfully updated.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
     updateArticle(
         @Param('id') articleID: number, 
         @Body('title') articleTitle: string, 
@@ -37,6 +47,8 @@ export class ArticleController{
     }
 
     @Delete(':id')
+    @ApiOkResponse({ description: 'The Resource has been successfully removed.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
     removeArticle(@Param('id') articleID: number){
         this.articleService.deleteArticle(articleID)
         return null;
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,30 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { ApiAcceptedResponse, ApiCreatedResponse, ApiForbiddenResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { UserService } from "./user.service";
 
+@ApiTags('user')
 @Controller('user')
 export class UserController{
-    constructor(private readonly userService: UserService){}
-    
+    constructor(private readonly userService: UserService){} 
+
+    @Get()
+    @ApiOkResponse({ description: 'The Resource list has been successfully returned.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
+    async getAllUser(){
+        const users = await this.userService.getUser();
+        return users;
+    }
+
+    @Get(':username')
+    @ApiOkResponse({ description: 'The Resource has been successfully returned.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
+    getUser(@Param('username')userUsername: string){
+        return this.userService.getSingleUser(userUsername)
+    }
+
     @Post()
+    @ApiCreatedResponse({ description: 'The Resource has been successfully created.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
     async addUser(
         @Body('username') userUsername: string,
         @Body('password') userPassword: string,
@@ -15,18 +34,9 @@ export class UserController{
         return{id: generatedID};
     }
 
-    @Get()
-    async getAllUser(){
-        const users = await this.userService.getUser();
-        return users;
-    }
-
-    @Get(':username')
-    getUser(@Param('username')userUsername: string){
-        return this.userService.getSingleUser(userUsername)
-    }
-
     @Patch(':username')
+    @ApiOkResponse({ description: 'The Resource has been successfully updated.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
     async updateUser(
         @Param('username') userUsername : string,
         @Body('password') userPassword : string,
@@ -36,7 +46,9 @@ export class UserController{
         return null;
     }
     
-    @Delete(':id')
+    @Delete(':username')
+    @ApiOkResponse({ description: 'The Resource has been successfully removed.' })
+    @ApiForbiddenResponse({ description: 'Forbidden.' })
     async removeUser(@Param('id') userUsername: string){
         await this.userService.deleteArticle(userUsername);
         return null;
